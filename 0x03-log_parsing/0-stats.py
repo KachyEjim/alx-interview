884326a6e2fe0fe5569ec7ab99b7b3b0de307d51
kachyEjim <ejimovcgmail.com>
2024-08-16 23:12:00 +0100

    Done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,50 @@
 #!/usr/bin/python3
-"""Script to parse log data and compute metrics."""
-
+"""Log Parser"""
 import sys
-import re
-
-# Dictionary to store the count of each status code
-STATUS_CODE = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-
-regex = (
-    r"^(\d{1,3}\.){3}\d{1,3}\s-\s\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\]"
-    r'\s"GET /projects/\d+ HTTP/1\.1"\s(\d{3})\s(\d+)$'
-)
-
-# Initialize counters
-count = 0
-size = 0
-
-
-def print_metrics():
-    """Function to print the accumulated metrics."""
-    print(f"File size: {size}")
-    for status, value in sorted(STATUS_CODE.items()):
-        if value > 0:
-            print(f"{status}: {value}")
 
 
 if __name__ == "__main__":
+    file_size = [0]
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+
+    def print_stats():
+        """Print statistics"""
+        print("File size: {}".format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print("{}: {}".format(key, status_codes[key]))
+
+    def parse_line(line):
+        """Checks the line for matches"""
+        try:
+            # Remove the new line character
+            line = line[:-1]
+            word = line.split(" ")
+            file_size[0] += int(word[-1])
+            status_code = word[-2]
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    line_count = 1
     try:
         for line in sys.stdin:
-            match = re.match(regex, line)
-            if not match:
-                continue
-
-            status_code = match.group(2)
-            file_size = match.group(3)
-
-            size += int(file_size)
-
-            if status_code in STATUS_CODE:
-                STATUS_CODE[status_code] += 1
-
-            count += 1
-
-            if count == 10:
-                print_metrics()
-                count = 0
-
+            parse_line(line)
+            """ print after every 10 lines """
+            if line_count % 10 == 0:
+                print_stats()
+            line_count += 1
     except KeyboardInterrupt:
-        print_metrics()
+        print_stats()
         raise
+    print_stats()
