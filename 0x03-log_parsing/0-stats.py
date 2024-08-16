4bf75a3646d131f5733426fb6547cf780f4273b8
kachyEjim <ejimovcgmail.com>
2024-08-16 22:37:04 +0100

    All task done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
-"""Log Parser"""
+"""Python file"""
 import sys
+import re
 
+STATUS_CODE = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+regex = (
+    r"^(\d{1,3}\.){3}\d{1,3}\s-\s\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+\]"
+    r'\s"GET /projects/\d+ HTTP/1\.1"\s(\d{3})\s(\d+)$'
+)
+count = 0
+size = 0
+
+
+def print_func():
+    """Print function"""
+    print(f"File size: {size}")
+    for status, value in STATUS_CODE.items():
+        if value:
+            print(f"{status}: {value}")
+
+
+print("here")
 if __name__ == "__main__":
-    file_size = [0]
-    status_codes = {200: 0, 301: 0,
-                    400: 0, 401: 0, 403: 0,
-                    404: 0, 405: 0, 500: 0}
-
-    def print_stats():
-        """Print statistics"""
-        print("File size: {}".format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print("{}: {}".format(key, status_codes[key]))
-
-    def parse_line(line):
-        """Checks the line for matches"""
-        try:
-            line = line[:-1]
-            word = line.split(" ")
-            file_size[0] += int(word[-1])
-            status_code = int(word[-2])
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
-
-    linenum = 1
     try:
         for line in sys.stdin:
-            parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
+            count += 1
+            match = re.match(regex, line)
+            if not re.match(regex, line):
+                continue
+            file_size = match.group(3)
+            status_code = match.group(2)
+            size += int(file_size)
+
+            if status_code in STATUS_CODE.keys():
+                STATUS_CODE[status_code] += 1
+                if count == 10:
+                    print_func()
+                    count = 0
+
     except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+        print_func()
+        count = 0
